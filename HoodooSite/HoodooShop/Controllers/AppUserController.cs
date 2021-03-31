d7ac0500c650c8e755a295a372966d655b3dc775
Aaron Richards <63485250+KlosheKopa@users.noreply.github.com>
2021-03-31 14:16:14 -0700

    Revert "models"

--- HoodooSite/HoodooShop/Controllers/AppUserController.cs
+++ HoodooSite/HoodooShop/Controllers/AppUserController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HillsideShop.Controllers
 {
-    public class AppUserController : Controller
+    public class UserController : Controller
     {
         public IActionResult Index()
         {
